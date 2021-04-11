95694c0884a47b74a1806419e43cc0d95de26463
bosd <evbokhoven@gmail.com>
2021-04-11 02:17:45 +0200

    [MIG] website_sale_product_description: Migration to 14.0

--- website_sale_product_description/__manifest__.py
+++ website_sale_product_description/__manifest__.py
@@ -4,7 +4,15 @@
     "name": "Website Sale Product Description",
     "category": "Website",
     "summary": "Shows custom e-Commerce description for products",
+<<<<<<< HEAD
     "version": "13.0.1.0.0",
+=======
+<<<<<<< HEAD
+    "version": "14.0.1.0.0",
+=======
+    "version": "13.0.1.0.0",
+>>>>>>> d81284d9... [MIG] website_sale_product_description: Migration to 13.0
+>>>>>>> 4f48128d... [MIG] website_sale_product_description: Migration to 13.0
     "website": "https://github.com/OCA/e-commerce",
     "author": "Solvos, Odoo Community Association (OCA)",
     "license": "AGPL-3",
@@ -13,4 +21,29 @@
     "depends": ["website_sale"],
     "data": ["views/website_sale_template.xml", "views/product_template.xml"],
     "demo": ["data/demo_website_sale_product_description.xml"],
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+    'name': 'Website Sale Product Description',
+    'category': 'Website',
+    'summary': 'Shows custom e-Commerce description for products',
+    'version': '12.0.1.0.0',
+    'website': 'https://github.com/OCA/e-commerce',
+    'author': 'Solvos, Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'depends': ['website_sale'],
+    'data': [
+        'views/website_sale_template.xml',
+        'views/product_template.xml',
+    ],
+    'demo': [
+        'data/demo_website_sale_product_description.xml',
+    ],
+>>>>>>> [ADD] website_sale_product_description
+=======
+>>>>>>> d81284d9... [MIG] website_sale_product_description: Migration to 13.0
+>>>>>>> 4f48128d... [MIG] website_sale_product_description: Migration to 13.0
 }
